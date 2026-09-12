3b415fa2b7862b3616b8c1506a592af6b7ada0b1
lovesonbk30-ui <lovesonbk30@gmail.com>
2026-09-12 19:45:44 +0545

    Update app.py

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ def message():
 			new_quest= Quest(message=user_msg)
 			db.session.add(new_quest)
 			db.session.commit()
+		    flash('Message sent!')
 	return redirect('/profile')
 			
 @app.route('/About')
