f01ad02cb94fc40c9c6e3ebcf22e917a291e5471
lovesonbk30-ui <lovesonbk30@gmail.com>
2026-09-13 19:14:23 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect, request, url_for, render_template_string, session, flash
+from flask import Flask, redirect, request, url_for, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -39,13 +39,13 @@ def home():
 		else:
 			flash('⛔️ Invalid credentials', 'danger')
 			return redirect('/')
-	return render_template_string(html)
+	return render_template('Login_Page.html')
 	
 @app.route('/profile')
 def profile():
 	if not session.get('logged_in'):
 		return redirect('/')
-	return render_template_string(Main, message=message)
+	return render_template('Potfolio_Page.html', message=message)
 	
 @app.route('/logout')
 def logout():
@@ -64,7 +64,7 @@ def message():
 	
 @app.route('/send')
 def add():
-				return render_template_string(About_Page)
+	return render_template('About.html')
 				
 
 if __name__ == '__main__':
